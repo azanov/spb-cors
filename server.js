d47f1539bb7209ad84bda869c13c1dbbf8bbd44a
Pavel Azanov <pavel@azanov.de>
2021-08-07 21:34:36 +0200

    up

--- server.js
+++ server.js
@@ -46,7 +46,8 @@ cors_proxy.createServer({
   },
   setHeaders: {
     origin: 'sportpaybar.com',
-    referrer: 'https://sportpaybar.com/ufc.html'
+    referrer: 'https://sportpaybar.com/ufc.html',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
   }
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
